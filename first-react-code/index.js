c26834e1cbf2719495998da8529746122ea2edd9
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-02-08 19:51:08 +0500

    react-first-code finished

--- first-react-code/index.js
+++ first-react-code/index.js
@@ -1,2 +1,8 @@
 const root = document.getElementById("root");
-ReactDOM.render(<h1>Lone Wolf</h1>, root);
+const list = <ul>
+    <li>list1</li>
+    <li>list2</li>
+</ul>;
+const arr = [<h1>LoneWolf</h1>,list];
+ReactDOM.render( arr , root);
+
