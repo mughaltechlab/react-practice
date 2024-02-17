9b542e7d7ad9a6596fcf046a10f20c417f01aa0e
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-02-18 01:10:44 +0500

    hover effect in tailwind with react

--- react-app/src/components/Nav.jsx
+++ react-app/src/components/Nav.jsx
@@ -5,10 +5,10 @@ function Nav(){
         <nav className="flex justify-between items-center px-5  shadow h-16 bg-white">
             <a href="" className="brand text-slate-700 font-bold">MughalTechLab</a>
             <ul className="flex gap-2">
-                <li className=""><a className="" href="">HOME</a></li>
-                <li className=""><a className="" href="">PROJECTS</a></li>
-                <li className=""><a className="" href="">GITHUB</a></li>
-                <li className=""><a className="" href="">CONTACT</a></li>
+                <li className="hover:bg-purple-500"><a className="" href="">HOME</a></li>
+                <li className="hover:bg-purple-500"><a className="" href="">PROJECTS</a></li>
+                <li className="hover:bg-purple-500"><a className="" href="">GITHUB</a></li>
+                <li className="hover:bg-purple-500"><a className="" href="">CONTACT</a></li>
             </ul>
         </nav>
     )
