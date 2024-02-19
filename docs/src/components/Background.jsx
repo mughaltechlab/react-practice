5409f9c65969f544d23af44e69ecaef5410290d7
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-02-19 12:17:21 +0500

    background and foreground components finished

---
+++ docs/src/components/Background.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Background() {
+  return (
+    <div className="relative w-full h-screen">
+      <h1 className="text-[20vw] text-zinc-500/10 leading-none tracking-tighter absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
+        Docs
+      </h1>
+    </div>
+  );
+}
+
+export default Background;
---
+++ docs/src/components/Foreground.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Foreground() {
+  return (
+    <div className='fixed w-full h-screen top-0 left-0 z-[2]'></div>
+  )
+}
+
+export default Foreground
