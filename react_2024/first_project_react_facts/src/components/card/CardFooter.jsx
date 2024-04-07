3863dd9fd5b84b0e434ca7f4234ca9b4da83cc2b
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-04-07 14:04:46 +0500

    layout fix (on mobile view)

--- react_2024/first_project_react_facts/src/components/card/CardFooter.jsx
+++ react_2024/first_project_react_facts/src/components/card/CardFooter.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function CardFooter() {
   return (
     <div className='cardFooter'>
-        <div className="social"><i class="fa-brands fa-twitter"></i></div>
-        {/* <div className="social"><i class="fa-brands fa-linkedin" aria-hidden="true"></i></div> */}
-        <div className="social"><i class="fa-brands fa-facebook-f"></i></div>
-        <div className="social"><i class="fa-brands fa-instagram"></i></div>
-        <div className="social"><i class="fa-brands fa-github"></i></div>
+        <div className="social"><i className="fa-brands fa-twitter"></i></div>
+        {/* <div className="social"><i className="fa-brands fa-linkedin" aria-hidden="true"></i></div> */}
+        <div className="social"><i className="fa-brands fa-facebook-f"></i></div>
+        <div className="social"><i className="fa-brands fa-instagram"></i></div>
+        <div className="social"><i className="fa-brands fa-github"></i></div>
     </div>
   )
 }
