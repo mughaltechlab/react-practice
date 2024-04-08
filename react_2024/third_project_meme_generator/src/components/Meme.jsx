7c302d5604ac93049c27427d6bb782c9bb36aa1d
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-04-08 22:57:37 +0500

    re-render the UI component by using useState hooks in react js

--- react_2024/third_project_meme_generator/src/components/Meme.jsx
+++ react_2024/third_project_meme_generator/src/components/Meme.jsx
@@ -1,21 +1,34 @@
-import {useState} from 'react'
+import React, {useState} from 'react'
 import memesData from './memesData';
 
-function getMemeImg(){
-    const memeDataLength = memesData.data.memes.length;
-    const rand = Math.random() * memeDataLength;
-    const randInd = Math.floor(rand);
-    const randMeme = memesData.data.memes[randInd];
-    // const url = randMeme.url;
-    const {url} = randMeme;
-    console.log(url)
-}
+
+
+
 
 
 function Meme() {
+    
+    //? useState  hook must be call only in functional component. "useState = State,use" (such as, [countVarhasValueZero, useMethodInWhichUseCountVar] = useState(0));
+    
+    // let [count, counter] = useState(0);
+    let [url,setUrl] = useState('');
+    function getMemeImg(){
+
+        // counter(count+=1);
+        // console.log(count);
+        const memeDataLength = memesData.data.memes.length;
+        const rand = Math.random() * memeDataLength;
+        const randInd = Math.floor(rand);
+        const randMeme = memesData.data.memes[randInd];
+        const randUrl = randMeme.url;
+        setUrl(url = randUrl)
+        console.log(url)
+    }
+    
   return (
     <main>
         <div className='form'>
+            {/* <div className="count">{url}</div> */}
             <input 
                 type="text"
                 className='formInp'
@@ -33,8 +46,10 @@ function Meme() {
             >
                 Get a new meme image 🖼
             </button>
+        <div className="memeImg">
+            <img src={url} alt="" />
         </div>
-        <div className="memeImg"></div>
+        </div>
     </main>
   )
 }
